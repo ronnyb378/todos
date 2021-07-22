82b935a0921c04db9e8ba0e12d6b5ffd0ed02e6d
Ronny Barahona <ronnybarahona@Ronnys-MacBook-Air.local>
2021-07-22 09:12:24 -0500

    buttons for completed and remove

--- public/script.js
+++ public/script.js
@@ -1,62 +1,83 @@
 function renderTodos(todosArray) {
-    const todosHtmlArray = todosArray.map(todo => {
-        return `<li class="${todo.completed ? 'completed' : 'incomplete'}">${todo.text}
-        <button class="delete-button" data-id="${todo.id}">x</button>
-        </li>`
-    })
-    return todosHtmlArray.join('')
+    const todosHtmlArray = todosArray.map((todo) => {
+        return `<li class="${todo.completed ? "completed" : "incomplete"}">
+        ${todo.text}
+        <button class="complete-button" data-id="${todo.id}"data-completed="${todo.completed ? "completed" : "incomplete"}">E</button>
+        <button class="delete-button" data-id="${todo.id}">X</button></li>`;
+    });
+
+    return todosHtmlArray.join("");
 }
 
 function fetchTodos() {
-    fetch('/api/v1/todos')
-        .then(res => res.json())
-        .then(data => {
-            // console.log(data)
-            todos.innerHTML = renderTodos(data)
-        })
+    fetch("/api/v1/todos")
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            todos.innerHTML = renderTodos(data);
+        });
 }
 
-const todos = document.getElementById('todos')
-const todosForm = document.getElementById('todoForm')
+const todos = document.getElementById("todos");
+const todoForm = document.getElementById("todoForm");
 
-fetchTodos()
+fetchTodos();
 
-todosForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    // console.log('test')
-    const input = document.getElementById('todo_text')
-    fetch('/api/v1/todos', {
-        method: 'POST', 
+todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input = document.getElementById("todo_text");
+    fetch("/api/v1/todos", {
+        method: "POST",
         headers: {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json",
         },
         body: JSON.stringify({
-            text: input.value
-        })
+            text: input.value,
+        }),
     })
-        .then(res => res.json())
-        .then(data => {
+        .then((res) => res.json())
+        .then((data) => {
             if (data.error) {
-                alert(data.error)
+                alert(data.error);
             }
-            fetchTodos()
-            todosForm.reset()
-        })
-})
+            fetchTodos();
+            todoForm.reset();
+        });
+});
 
-document.addEventListener('click', (e) => {
-    if (e.target.classList.contains('delete-button')) {
-        // delete behavior
+document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("delete-button")) {
         const id = e.target.dataset.id;
         fetch(`/api/v1/todos/${id}`, {
-            method: 'DELETE',
+            method: "DELETE",
         })
-            .then(res => !res.ok && res.json())
-            .then(data => {
+            .then((res) => !res.ok && res.json())
+            .then((data) => {
                 if (data.error) {
-                    alert(data.error)
+                    alert(data.error);
                 }
-                fetchTodos()
-            })
+                fetchTodos();
+            });
     }
-})
+
+    if (e.target.classList.contains("complete-button")) {
+        const id = e.target.dataset.id;
+        const completed = e.target.dataset.completed;
+        fetch(`/api/v1/todos/${id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                completed: completed === "completed" ? false : true,
+            }),
+        })
+            .then((res) => !res.ok && res.json())
+            .then((data) => {
+                if (data.error) {
+                    alert(data.error);
+                }
+                fetchTodos();
+            });
+    }
+});
