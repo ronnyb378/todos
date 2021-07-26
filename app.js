6b9cbdea7834a2993eb78ea44f80fb669c35e747
Ronny Barahona <ronnybarahona@Ronnys-MacBook-Air.local>
2021-07-26 15:11:03 -0500

    updated the app to use proper database instead of local db

--- app.js
+++ app.js
@@ -1,91 +1,99 @@
 //todo | require 
 const http = require("http");
-const express = require("express")
-const db = require("./model/db");
+const express = require("express");
+const pgp = require("pg-promise")();
+// const db = require("./model/db");
+const config = require("./config")
+const db = pgp(config)
 
 //todo | set up server 
 const app = express();
 const server = http.createServer(app)
 
-let id = 6;
-
 //todo | include middleware (static files, json, urlencoded)
 app.use(express.static('./public'))
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
 //todo | get all todos
-app.get('/api/v1/todos', (req,res) => {
-    res.json(db.todos)
+app.get('/api/v1/todos', (req, res) => {
+    db.many("SELECT * FROM todos")
+        .then((todos) => {
+            res.json(todos)
+        })
 })
 
 //todo | create new todos 
-app.post('/api/v1/todos' , (req, res) => {
+app.post('/api/v1/todos', (req, res) => {
     //todo | console.log(req.body)
     if (!req.body || !req.body.text) {
         // respond with an error
         res.status(422).json({
-            error: "are u stupid? add fucking text"
+            error: "add text"
         })
         return
     }
-    const newTodo= {
-        id: id++,
-        text: req.body.text,
-        completed: false,
+    const newTodo = {
+        text: req.body.text
     }
-    db.todos.push(newTodo)
-    res.status(201).json(newTodo)
+    db.result("INSERT INTO todos (text) VALUES (${text}) RETURNING *", newTodo)
+        .then((results) => {
+            const createdTodo = results.row[0]
+            res.status(201).json(createdTodo)
+        })
 })
 
-//todo | update existing todo by id
-app.patch('/api/v1/todos/:id', (req,res) => {
-    //todo |get the id from the route
+//todo | Update existing todo by id
+app.patch('/api/v1/todos/:id', (req, res) => {
+    // get the id from the route
     const id = parseInt(req.params.id)
-    //find the existing todo
-    const todoIndex = db.todos.findIndex((todo) => {
-        return todo.id === id
-    })
-    //todo | if we could not find the todo with that id
-    if (todoIndex === -1) {
-        res.status(404).json({ error: 'Could not find todo with that id'})
-        return
-    }
-    //todo |update the text/completed
+    let data = { id: id }
+    let query = "UPDATE todos SET "
+
     if (req.body && req.body.text) {
-        db.todos[todoIndex].text = req.body.text
+        query += "text = ${text}"
+        data.text = req.body.text
     }
+
+    if (req.body && req.body.text && req.body.completed !== undefined) {
+        query += ', '
+    }
+
     if (req.body && req.body.completed !== undefined) {
-        db.todos[todoIndex].completed = req.body.completed
-    } 
-    //todo |respond with updated item
-    res.json(db.todos[todoIndex])
+        query += "completed = ${completed}"
+        data.completed = req.body.completed
+    }
+    query += " WHERE id = ${id}"
+    // console.log(query);
+
+    db.result(query, data)
+        .then(results => {
+            if (results.rowCount === 0) {
+                res.status(404).json({ error: 'could not find todo with that id ' })
+            } else {
+                res.json()
+            }
+        })
 })
 
 //todo | delete existing todo by id 
 app.delete('/api/v1/todos/:id', (req, res) => {
     //todo | get the id
     const id = parseInt(req.params.id)
-    //todo | find the existing todo
-    const todoIndex = db.todos.findIndex((todo) => {
-        return todo.id === id
-    })
-    //todo | if we could not find the todo with that id
-    if (todoIndex === -1) {
-        res.status(404).json({ error: 'Could not find todo with that id'})
-        return
-    }
-    //todo | delete the todo
-    db.todos.splice(todoIndex, 1)
 
-    // db.todos = db.todos.filter((todo) => {
-    //     return todo.id !== id
-    // })
-    //todo | respond with 204 status
-    res.status(204).json()
+    //todo | find the existing todo
+    db.result("DELETE FROM todos WHERE id = $1", id)
+        .then((results) => {
+            if (results.rowCount === 0) {
+                res.status(404).json({ error: 'Could not find todo with that id' })
+            } else {
+                res.status(204).json()
+            }
+        })
 })
 
 //todo | listen for request
 server.listen(3000, '127.0.0.1', () => {
     console.log('Server running: http://127.0.0.1:3000')
+
 })
---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+    host: "localhost",
+    port: "5432",
+    database: "todos",
+    user: "postgres"
+}
+
+module.exports = config
